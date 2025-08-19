8f92880234f87da02db5aead82d048823206c59e
ptakondrej <florixakcz@seznam.cz>
2025-08-19 15:06:18 +0200

    Initialize Spring Boot backend project
    Add initial Spring Boot backend setup with Maven wrapper, project structure, dependencies (Spring Boot, JPA, Security, JWT, PostgreSQL, Lombok), application properties, and a basic application entry point. Includes initial test class and IDE configuration files.

---
+++ backend/src/main/java/me/ptakondrej/minieshop/MiniEshopApplication.java
@@ -0,0 +1,13 @@
+package me.ptakondrej.minieshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniEshopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/me/ptakondrej/minieshop/MiniEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ptakondrej.minieshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
