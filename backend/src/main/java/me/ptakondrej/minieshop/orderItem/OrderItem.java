5235b0f777c15a7b9be35e260abc72c78a66e5f8
ptakondrej <florixakcz@seznam.cz>
2025-08-20 21:19:55 +0200

    Add OrderItemMapper and update OrderItem with builder
    Introduced OrderItemMapper for converting between OrderItem and OrderItemDTO. Updated OrderItem entity to use Lombok's @Builder annotation for easier object construction.

--- backend/src/main/java/me/ptakondrej/minieshop/orderItem/OrderItem.java
+++ backend/src/main/java/me/ptakondrej/minieshop/orderItem/OrderItem.java
@@ -1,10 +1,7 @@
 package me.ptakondrej.minieshop.orderItem;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import me.ptakondrej.minieshop.order.Order;
 import me.ptakondrej.minieshop.product.Product;
 
@@ -14,17 +11,21 @@ import me.ptakondrej.minieshop.product.Product;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "order_items")
+@Builder
 public class OrderItem {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	@ManyToOne
 	@JoinColumn(name = "order_id", nullable = false)
 	private Order order;
+
 	@ManyToOne
 	@JoinColumn(name = "product_id", nullable = false)
 	private Product product;
+
 	@Column(nullable = false, columnDefinition = "integer default 0")
 	private Integer quantity;
 }
---
+++ backend/src/main/java/me/ptakondrej/minieshop/orderItem/OrderItemMapper.java
@@ -0,0 +1,23 @@
+package me.ptakondrej.minieshop.orderItem;
+
+import me.ptakondrej.minieshop.models.OrderItemDTO;
+import me.ptakondrej.minieshop.product.ProductMapper;
+
+public class OrderItemMapper {
+
+	public static OrderItemDTO convertToDto(OrderItem orderItem) {
+		return OrderItemDTO.builder()
+				.id(orderItem.getId())
+				.product(ProductMapper.convertToDto(orderItem.getProduct()))
+				.quantity(orderItem.getQuantity())
+				.build();
+	}
+
+	public static OrderItem convertToEntity(OrderItemDTO orderItemDTO) {
+		return OrderItem.builder()
+				.id(orderItemDTO.getId())
+				.product(ProductMapper.convertToEntity(orderItemDTO.getProduct()))
+				.quantity(orderItemDTO.getQuantity())
+				.build();
+	}
+}
