2037729d1f74905009815f6dbe03a1015f86fe3e
ptakondrej <florixakcz@seznam.cz>
2025-09-03 20:01:57 +0200

    Update Header navigation and props
    Changed the 'Shop' link href from '/products' to '/shop', updated the home link search params, and improved Link components with active/inactive props. Also removed the SearchBar component from the Header.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,4 @@
 import { Link } from "@tanstack/react-router";
-import SearchBar from "./SearchBar";
 import { Heart, Menu, ShoppingCart, User } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
@@ -12,7 +11,7 @@ const navLinks = [
   },
   {
     title: "Shop",
-    href: "/products",
+    href: "/shop",
   },
   {
     title: "About",
@@ -28,7 +27,10 @@ const Header = () => {
   const cartItems = 3;
   return (
     <header className="fixed flex flex-row items-center justify-between px-6 md:px-16 lg:px-36 py-4 bg-white shadow-sm font-playfair w-full z-50">
-      <Link to="/" search={{ category: "all", filter: "no-filter" }}>
+      <Link
+        to="/"
+        search={{ category: "all", sortBy: "no-filter", view: "grid" }}
+      >
         <h1 className="text-2xl font-bold text-primary">Minimal</h1>
       </Link>
       <nav className="hidden lg:flex flex-row gap-10 items-center">
@@ -36,14 +38,16 @@ const Header = () => {
           <Link
             key={link.title}
             to={link.href}
-            className="text-primary hover:text-secondary-200"
+            activeProps={{ className: "font-bold text-primary cursor-default" }}
+            inactiveProps={{
+              className: "text-primary hover:text-secondary-200",
+            }}
           >
             {link.title}
           </Link>
         ))}
       </nav>
       <div className="flex flex-row gap-2 md:gap-6 items-center">
-        <SearchBar className="hidden md:block" />
         <Button
           variant="ghost"
           size="icon"
