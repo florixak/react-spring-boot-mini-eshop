8977ed360d76f91cf7448c7037961fe5aa1bde3e
ptakondrej <florixakcz@seznam.cz>
2025-09-04 09:43:11 +0200

    Rearrange FAQ card layout in SendMessage component
    Moved the FAQ card to span two columns at the bottom of the grid and adjusted its placement for improved layout on larger screens. Removed the previous FAQ card position and updated grid classes for better responsiveness.

--- frontend/src/components/SendMessage.tsx
+++ frontend/src/components/SendMessage.tsx
@@ -14,7 +14,7 @@ const SendMessage = () => {
       id="contact"
     >
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full max-w-4xl h-auto">
-        <Card className="row-span-2 p-4">
+        <Card className="p-4">
           <CardHeader className="text-left p-2">
             <h3 className="text-2xl font-semibold text-primary">
               Contact Form
@@ -24,18 +24,6 @@ const SendMessage = () => {
             </p>
           </CardHeader>
           <ContactForm />
-        </Card>
-        <Card className="p-4">
-          <CardHeader className="text-left p-2">
-            <h3 className="text-2xl font-semibold text-primary flex items-center">
-              <CircleQuestionMark className="inline-block mr-2" />
-              Frequently Asked Questions
-            </h3>
-            <p className="text-secondary-200 mt-1">
-              Quick answers to common questions
-            </p>
-          </CardHeader>
-          <Faq />
         </Card>
         <Card className="p-4">
           <CardHeader className="text-left p-2">
@@ -67,6 +55,18 @@ const SendMessage = () => {
               Live Chat (9AM-8PM EST)
             </Button>
           </CardContent>
+        </Card>
+        <Card className="p-4 row-span-1 md:col-span-2">
+          <CardHeader className="text-left p-2">
+            <h3 className="text-2xl font-semibold text-primary flex items-center">
+              <CircleQuestionMark className="inline-block mr-2" />
+              Frequently Asked Questions
+            </h3>
+            <p className="text-secondary-200 mt-1">
+              Quick answers to common questions
+            </p>
+          </CardHeader>
+          <Faq />
         </Card>
       </div>
     </Section>
