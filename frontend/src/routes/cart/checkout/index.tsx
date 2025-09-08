48eacaca399d7e2152e58a05849859d326584e40
ptakondrej <florixakcz@seznam.cz>
2025-09-08 07:20:43 +0200

    Add validation and loader to checkout route
    Introduces search parameter validation and a loader to the checkout route. Ensures the 'step' parameter is a valid number within the allowed range and redirects if invalid. Adds support for an optional 'order_id' parameter and validates its presence in the loader.

--- frontend/src/routes/cart/checkout/index.tsx
+++ frontend/src/routes/cart/checkout/index.tsx
@@ -1,13 +1,37 @@
 import CheckoutContent from "@/components/checkout/CheckoutContent";
 import CheckoutHeader from "@/components/checkout/CheckoutHeader";
 import { CHECKOUT_STEPS } from "@/constants";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+type CheckoutSearch = {
+  step: number;
+  order_id?: string;
+};
 
 export const Route = createFileRoute("/cart/checkout/")({
   component: Checkout,
-  validateSearch: (search) => ({
+  validateSearch: (search): CheckoutSearch => ({
     step: search.step ? Number(search.step) : 1,
+    order_id: search.order_id as string | undefined,
   }),
+  beforeLoad: ({ search }) => {
+    if (
+      (search.step && isNaN(Number(search.step))) ||
+      Number(search.step) < 1 ||
+      Number(search.step) > CHECKOUT_STEPS.length
+    ) {
+      throw redirect({ to: "/cart/checkout", search: { step: 1 } });
+    }
+  },
+  loaderDeps: ({ search: { order_id } }) => ({
+    order_id,
+  }),
+  loader: async ({ deps: { order_id } }) => {
+    if (order_id) {
+      return { orderValid: true };
+    }
+    return { orderValid: false };
+  },
 });
 
 function Checkout() {
