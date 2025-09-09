056d856aeb3077b23c9cae5869211b3a3bc1d6c6
ptakondrej <florixakcz@seznam.cz>
2025-09-09 18:09:49 +0200

    Remove useAuthCheck hook
    Removed due to sending request too many requests to backend

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,11 +1,9 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import useAuthCheck from "@/hooks/useAuthCheck";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 function RouteComponent() {
-  useAuthCheck();
   return (
     <div className="min-h-screen overflow-x-hidden">
       <Header />
